55e0da84a151cfaccd31887b465023b0992d7927
Aqdas <farzanashadab.786@gmail.com>
2026-07-08 06:39:51 +0530

    Fix button click event listener syntax

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const button =
   document.querySelector("button");
 
-button.addEventListener("click"),
-  function () {
+button.addEventListener("click",
+ function () {
     alert("WELCOME TO MY PORTFOLIO! 🚀");
   });
